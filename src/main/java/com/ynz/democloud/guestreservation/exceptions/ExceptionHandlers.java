266bd32505bd5964e7fcfb744bca746136f951f2
yichunzhao <zhaoyichun@hotmail.com>
2020-07-22 22:46:07 +0200

    removing un-used exception

--- src/main/java/com/ynz/democloud/guestreservation/exceptions/ExceptionHandlers.java
+++ src/main/java/com/ynz/democloud/guestreservation/exceptions/ExceptionHandlers.java
@@ -4,7 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class ExceptionHandlers {
@@ -12,11 +11,6 @@ public class ExceptionHandlers {
     @ExceptionHandler
     public ResponseEntity<String> NotFoundExceptionHandler(NotFoundException e) {
         return new ResponseEntity(e.getMessage(), HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<String> IllegalArgumentExceptionHandler(MethodArgumentTypeMismatchException e) {
-        return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
 }
