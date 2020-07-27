7ce3129348928fb7a95751cad4534bf73379c64c
yichunzhao <zhaoyichun@hotmail.com>
2020-07-27 19:43:48 +0200

    registering services with Eureka

--- src/main/java/com/ynz/democloud/guestreservation/GuestReservationApplication.java
+++ src/main/java/com/ynz/democloud/guestreservation/GuestReservationApplication.java
@@ -3,8 +3,10 @@ package com.ynz.democloud.guestreservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GuestReservationApplication {
 
     public static void main(String[] args) {
